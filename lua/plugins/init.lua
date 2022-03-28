c6061ead4850d08f5783db66f2bf89c40bda1f76
Pierce Bartine <piercebartine@gmail.com>
2022-03-28 00:35:32 -0700

    Somewhat stable, catppuccin ts_rainbow not working

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -1,5 +1,40 @@
 local packer = require("packer")
 
-packer.startup(function(use)
-	use("wbthomason/packer.nvim")
-end)
+-- Initialize Packer
+packer.init({
+	profile = {
+		enable = false,
+		threshold = 0,
+	},
+})
+packer.reset()
+
+-- Load plugins
+local use = packer.use
+
+use("wbthomason/packer.nvim")
+
+use({
+	"nvim-treesitter/nvim-treesitter",
+	run = ":TSUpdate",
+	config = function()
+		require("plugins.treesitter")
+	end,
+})
+use("p00f/nvim-ts-rainbow")
+
+use({
+	"catppuccin/nvim",
+	as = "catppuccin",
+	config = function()
+		require("plugins.catppuccin")
+	end,
+})
+
+use({
+	"nvim-lualine/lualine.nvim",
+	requires = { "kyazdani42/nvim-web-devicons", opt = true },
+	config = function()
+		require("plugins.lualine")
+	end,
+})
