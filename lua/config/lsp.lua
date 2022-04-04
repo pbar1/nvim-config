866b24325d4d14fe10edaede06d6446f6149542c
Pierce Bartine <piercebartine@gmail.com>
2022-04-03 23:43:28 -0700

    Add automatic formatting

--- lua/config/lsp.lua
+++ lua/config/lsp.lua
@@ -1,5 +1,6 @@
 local lspconfig = require("lspconfig")
 local cmp_nvim_lsp = require("cmp_nvim_lsp")
+local null_ls = require("null-ls")
 
 local capabilities = cmp_nvim_lsp.update_capabilities(vim.lsp.protocol.make_client_capabilities())
 
@@ -34,4 +35,25 @@ lspconfig["sumneko_lua"].setup({
 			telemetry = { enable = false },
 		},
 	},
+})
+
+-- Configure formatters, linters, and diagnostic tools with null-ls
+null_ls.setup({
+	sources = {
+		null_ls.builtins.formatting.goimports, -- FIXME: Disable gopls formatting to avoid prompt
+		null_ls.builtins.formatting.black,
+		null_ls.builtins.formatting.stylua,
+	},
+
+	-- Format on save if supported
+	on_attach = function(client)
+		if client.resolved_capabilities.document_formatting then
+			vim.cmd([[
+            augroup LspFormatting
+                autocmd! * <buffer>
+                autocmd BufWritePre <buffer> lua vim.lsp.buf.formatting_sync()
+            augroup END
+            ]])
+		end
+	end,
 })
